2c670e85b35df63b8eac0776e84c2357e4bbad10
ABINEL MWANGI <kiraguabinel@outlook.com>
2023-03-31 04:26:39 +0300

    Not yet complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+//Default activity named "activity"
+saturdayFun()
+function saturdayFun(activity = 'roller-skate'){
+    return `This saturday, I want to ${activity}!`;
+}
+
+saturdayFun("bathe my dog")
+
+//Default activity is named "act"
+const mondayWork = function(act = "go to the office"){
+    return `This monday, I will ${act}.`;
+}
+
+mondayWork()
+
+mondayWork("work from home")
+
+//Lab 3 "wrapAdjective"
+/*const wrapAdjective = function (star = '*'){
+    return function(diet = 'special')
+
+//}*/
+
+/*function outer(greeting, msg = "It's a fine day to learn") {
+    return function (name, lang = "Python") {
+      return `${greeting}, ${name}! ${msg} ${lang}`;
+    };
+  }*/
